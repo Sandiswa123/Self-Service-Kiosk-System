bede02d408f5f0230b4310dd697314fc18d85333
Ntokozo18-pixel <226303377@student.uj.ac.za>
2026-09-21 18:54:15 +0200

    Add files via upload

---
+++ APIcheck/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace APIcheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNearestStore_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.google.com/maps/search/?api=1&query=supermarkets+in+Johannesburg";
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+
+            });
+        }
+    }
+}
